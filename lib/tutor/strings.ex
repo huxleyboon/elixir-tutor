0d98fafdc3f2b90f07bee23d805b0a20f95d69dc
Rob Phoenix <robertphoenix@gmail.com>
2016-03-10 11:19:46 +0000

    add strings tests

---
+++ lib/tutor/strings.ex
@@ -0,0 +1,151 @@
+defmodule Tutor.Strings do
+	@moduledoc """
+  A module of functions for manipulating Strings
+  """
+
+  @doc """
+  Converts all characters in the given string to lowercase.
+
+  ## Example
+
+      iex> Tutor.Strings.lowercase "Hello"
+      "hello"
+      iex> Tutor.Strings.lowercase "HELLO"
+      "hello"
+
+  """
+  def lowercase(str), do: String.downcase str
+
+  @doc """
+  Converts all characters in the given string to lowercase,
+  and then doubles the given string.
+
+  ## Example
+
+      iex> Tutor.Strings.lowercase_doubled "Hello"
+      "hellohello"
+      iex> Tutor.Strings.lowercase_doubled "HELLO"
+      "hellohello"
+
+  """
+  def lowercase_doubled(str) do
+    str
+    |> String.downcase
+    |> String.duplicate(2)
+  end
+
+  @doc """
+  Greets someone with the given name
+
+  ## Example
+
+      iex> Tutor.Strings.greet "Huxley Boon"
+      "Hello, Huxley Boon!"
+
+  """
+  def greet(name), do: "Hello, #{name}!"
+
+  @doc """
+  With a given first name and last name, returns the full name
+
+  ## Example
+
+      iex> Tutor.Strings.full_name("Rich", "Phoenix")
+      "Rich Phoenix"
+
+  """
+  def full_name(last, first) do
+    first <> " " <> last
+  end
+
+
+  @doc """
+  Checks if a given string contains the character a.
+
+  ## Example
+
+      iex> Tutor.Strings.has_an_a? "hello there"
+      false
+      iex> Tutor.Strings.has_an_a? "hallo there"
+      true
+
+  """
+  def has_an_a?(str) do
+    
+  end
+
+  @doc """
+  Checks if a given string is a question.
+  A string is defined as a question if it's last character
+  is a question mark
+
+  ## Example
+
+      iex> Tutor.Strings.question? "How are you?"
+      true
+      iex> Tutor.Strings.question? "Hello"
+      false
+
+  """
+  def question?(str) do
+    
+  end
+
+  @doc """
+  Replaces the string "Donald Trump" with "Someone With Tiny Hands"
+  [Reference](http://www.theverge.com/2016/3/9/11185484/donald-trump-chrome-extension-strikes-at-the-heart-of-media)
+
+  ## Example
+
+      iex> Tutor.Strings.trump("Donald Trump is running for President")
+      "Someone With Tiny Hands is running for President"
+
+  """
+  def trump(str) do
+    
+  end
+
+  @doc """
+  Returns a list of all the words in the given string,
+  where each word is reversed
+
+  ## Example
+
+      iex> Tutor.Strings.list_reversed "good day to you"
+      ["uoy", "ot", "yad", "doog"]
+
+  """
+  def list_reversed(str) do
+    
+  end
+
+  @doc """
+  Returns first two letters of a given string
+
+  ## Example
+
+      iex> Tutor.Strings.first_two "Hello"
+      "He"
+      iex> Tutor.Strings.first_two "How are you?"
+      "Ho"
+      iex> Tutor.Strings.first_two "  Hello"
+      "He"
+
+  """
+  def first_two(str) do
+    
+  end
+
+  @doc """
+  Returns the given string as an atom
+  """
+
+  @doc """
+  With a given name, and a given verb,
+  says what that person did today
+  """
+
+  @doc """
+  Returns any given string as a shouted question
+  """
+end
---
+++ test/strings_test.exs
@@ -0,0 +1,102 @@
+defmodule Tutor.StringsTest do
+	use ExUnit.Case
+
+  alias Tutor.Strings
+
+  # @tag :skip
+  test "converts string to lowercase" do
+    assert Strings.lowercase("Hello") == "hello"
+    assert Strings.lowercase("hello") == "hello"
+    assert Strings.lowercase("HELLO") == "hello"
+  end
+
+  # @tag :skip
+  test "converts string to lowercase and doubles it" do
+    assert Strings.lowercase_doubled("hello") == "hellohello"
+    assert Strings.lowercase_doubled("HELLO") == "hellohello"
+    assert Strings.lowercase_doubled("Hello") == "hellohello"
+  end
+
+  @tag :skip
+  test "greets person" do
+    assert Strings.greet("Jack") == "Hello, Jack!"
+  end
+
+  @tag :skip
+  test "returns full name" do
+    assert Strings.full_name("Phoenix", "Leila") == "Leila Phoenix"
+  end
+
+  @tag :skip
+  test "returns true if string contains the character a" do
+    assert Strings.has_an_a?("a") == true
+    assert Strings.has_an_a?("an elephant") == true
+    assert Strings.has_an_a?("sauna") == true
+    assert Strings.has_an_a?("sssssssssssssauna") == true
+  end
+
+  @tag :skip
+  test "returns false if string does not contain the character a" do
+    assert Strings.has_an_a?(" ") == false
+    assert Strings.has_an_a?("No") == false
+    assert Strings.has_an_a?("youth") == false
+  end
+
+  @tag :skip
+  test "returns true if the given string is a question" do
+    assert Strings.question?("Is this a question?") == true
+  end
+
+  @tag :skip
+  test "returns false if the given string is not a question" do
+    assert Strings.question?("This is not a question.") == false
+  end
+
+  @tag :skip
+  test "returns false if the given string is not a question but contains a question mark" do
+    assert Strings.question("Hello? Hello? Fine. Whatever.") == false
+  end
+
+  @tag :skip
+  test "replaces Donald Trump with Someone With Small Hands" do
+    paragraph = "Donald Trump said Wednesday that he thinks \"Islam hates us,\" drawing little distinction between the religion and radical Islamic terrorism. Asked if the hate was \"in Islam itself,\" Donald Trump would only say that was for the media to figure out."
+    expected = "Someone With Small Hands said Wednesday that he thinks \"Islam hates us,\" drawing little distinction between the religion and radical Islamic terrorism. Asked if the hate was \"in Islam itself,\" Someone With Small Hands would only say that was for the media to figure out."
+    assert Strings.trump("Donald Trump") == "Someone With Small Hands"
+    assert Strings.trump(paragraph) == expected
+  end
+
+  @tag :skip
+  test "returns a list of reversed words" do
+    assert Strings.list_reversed("Say it ain't so") == ["os", "t'nia", "ti", "yaS"]
+  end
+
+  @tag :skip
+  test "returns first two letters of given string" do
+    assert Strings.first_two("goodbye") == "go"
+  end
+
+  @tag :skip
+  test "returns first two letters of a given string, ignoring whitespace" do
+    assert Strings.first_two("     goodbye") == "go"
+  end
+
+  @tag :skip
+  test "returns given string as an atom" do
+    assert Strings.to_atom("huxleyboon") == :huxleyboon
+  end
+
+  @tag :skip
+  test "returns string with spaces as an atom without quotations" do
+    assert Strings.to_atom("huxley boon") == :huxley_boon
+  end
+
+  @tag :skip
+  test "returns what the given person did today" do
+    assert Strings.did_today("Nicole", "lost her phone") == "Today, Nicole lost her phone."
+  end
+
+  @tag :skip
+  test "turns normal string into shouted question" do
+    assert Strings.shout_question("hello") == "HELLO?"
+  end
+end
